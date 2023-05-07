94f84b55adff40a6279f328feefaed62cf4ca867
MahinMuhammad <amimahinurrahman@gmail.com>
2023-05-07 11:39:36 +0600

    WebView added to DetailView

---
+++ TechEveryday/TechEverydayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechEverydayApp.swift
+//  TechEveryday
+//
+//  Created by Md. Mahinur Rahman on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TechEverydayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
